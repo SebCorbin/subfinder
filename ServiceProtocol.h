4e4e642cb8c7bda712e39c28c81125738186bb24
Sebastien Corbin <seb.corbin@gmail.com>
2012-01-07 09:02:50 +0100

    Added documentation on the protocol for services

--- ServiceProtocol.h
+++ ServiceProtocol.h
@@ -9,18 +9,40 @@
 
 @protocol ServiceProtocol
 
+/**
+ * file : SubFileShow or SubFileMovie
+ * return an array of SubSource
+ */
 - (NSMutableArray *)searchSubtitlesForSubFile:(id)file;
 
+/**
+ * Actually creates the srt file
+ */
 + (void)downloadSubtitleForSource:(SubSource *)source;
 
+/**
+ * If the service handles movie subtitles
+ */
 + (BOOL)handlesMovies;
 
+/**
+ * If the service handles show subtitles
+ */
 + (BOOL)handlesShows;
 
+/**
+ * Returns an array of language codes handled by the service
+ */
 + (NSArray *)handleLanguages;
 
+/**
+ * Returns the name of the service
+ */
 + (NSString *)serviceName;
 
+/**
+ * Returns the URL of the service
+ */
 + (NSString *)serviceHost;
 
 
